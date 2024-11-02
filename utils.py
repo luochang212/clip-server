b337e7342ceb3f6a0fb7fd7420d1d6e9ff3ebd2d
Chang Luo <33987852+luochang212@users.noreply.github.com>
2024-11-02 23:14:46 +0800

    first commit

---
+++ utils.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+
+def gen_abspath(directory: str, rel_path: str) -> str:
+    """
+    Generate the absolute path by combining the given directory with a relative path.
+
+    :param directory: The specified directory, which can be either an absolute or a relative path.
+    :param rel_path: The relative path with respect to the 'dir'.
+    :return: The resulting absolute path formed by concatenating the absolute directory
+             and the relative path.
+    """
+    abs_dir = os.path.abspath(directory)
+    return os.path.join(abs_dir, rel_path)
+
+
+def read_csv(
+    file_path: str,
+    sep: str = ',',
+    header: int = 0,
+    on_bad_lines: str = 'warn',
+    encoding: str = 'utf-8',
+    dtype: dict = None,
+    **kwargs
+) -> pd.DataFrame:
+    """
+    Read a CSV file from the specified path.
+    """
+    return pd.read_csv(file_path,
+                       header=header,
+                       sep=sep,
+                       on_bad_lines=on_bad_lines,
+                       encoding=encoding,
+                       dtype=dtype,
+                       **kwargs)
+
